be7bfde62dfdd0ef1ec92f1b54b0f18965fbee3a
Ritesh Raj Moond <moondritesh@gmail.com>
2025-02-05 20:55:26 +0530

    Adding TORRENT files

--- main.mjs
+++ main.mjs
@@ -1,7 +1,29 @@
-'use strict';
+"use strict";
 
-import bencode from 'bencode';
-import fs from 'fs';
+import bencode from "bencode";
 
-const torrent = bencode.decode(fs.readFileSync('my_torrent.torrent'));
-console.log(torrent.announce.toString('utf8'));
+import fs from "fs";
+import dgram from "dgram";
+import { Buffer } from "buffer";
+
+const torrent = bencode.decode(fs.readFileSync("my_torrent.torrent"));
+
+const announceUrl = torrent.announce
+  .toString("utf8")
+  .split(",")
+  .map((code) => String.fromCharCode(code))
+  .join("");
+
+const url = new URL(announceUrl);
+
+const socket = dgram.createSocket("udp4");
+const myMsg = Buffer.from(
+  "Hare Krsna Hare Krsna Krsna Krsna Hare Hare Hare Rama Hare Rama Rama Rama Hare Hare",
+  "utf8"
+);
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+    console.log("Message recieved is: ", msg);
+})
