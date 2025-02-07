5af36792751089d26a2b5932d52f0759cbdd6521
Ritesh Raj Moond <moondritesh@gmail.com>
2025-02-07 20:15:50 +0530

    Added info-hash and size in torrent-parser

--- torrent-parser.mjs
+++ torrent-parser.mjs
@@ -7,8 +7,21 @@ const open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 };
 
-const size = (torrent) => {};
+const size = (torrent) => {
+  const size = torrent.info.files
+    ? torrent.info.files
+        .map((file) => BigInt(file.length))
+        .reduce((a, b) => a + b, BigInt(0))
+    : BigInt(torrent.info.length);
 
-const infoHash = (torrent) => {};
+  const buf = Buffer.alloc(8);
+  buf.writeBigUInt64BE(size, 0);
+  return buf;
+};
+
+const infoHash = (torrent) => {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash("sha1").update(info).digest();
+};
 
 export { open, size, infoHash };
